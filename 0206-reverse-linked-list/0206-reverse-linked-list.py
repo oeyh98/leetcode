a503b8a4540dc0658661a9ac67f243e7f563a1ec
jinhyeok <skusus123@gmail.com>
2024-03-23 13:09:07 +0900

    Time: 41 ms (29.48%), Space: 17.6 MB (76.70%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -1,3 +1,4 @@
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -5,12 +6,24 @@ class ListNode:
         
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        current, prev = head, None
+        stack = []
+        current = head
         
-        
+        # 리스트를 순회하면서 모든 노드를 스택에 삽입
         while current:
-            next_temp, current.next = current.next, prev # 위치 변경
-            prev, current = current, next_temp # 다음 node setting
-
+            stack.append(current)
+            current = current.next
             
-        return prev
+        # 스택이 비어 있으면, 빈 리스트를 반환
+        if not stack:
+            return None
+        
+        # 스택에서 노드를 꺼내어 연결 리스트를 재구성
+        new_head = stack.pop()
+        current = new_head
+        while stack:
+            current.next = stack.pop()
+            current = current.next
+        current.next = None  # 마지막 노드의 next를 None으로 설정
+        
+        return new_head
