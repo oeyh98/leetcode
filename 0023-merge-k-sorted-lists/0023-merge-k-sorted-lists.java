4620fec31a8ccf828c77b9e4427efe2937658204
jinhyeok <skusus123@gmail.com>
2024-04-09 13:47:02 +0900

    Time: 4 ms (71.86%), Space: 44.8 MB (15.24%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,24 +9,35 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+class KComparator implements Comparator<ListNode> {
+    @Override
+    public int compare(ListNode o1, ListNode o2){
+        if(o1.val == o2.val) return 0;
+        else if (o1.val > o2.val) return 1;
+        else return -1;
+    }
+}
+
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(new KComparator());
+        
         ListNode head = new ListNode();
         ListNode temp = head;
-        List<Integer> arr = new ArrayList<>();
         
-        for(ListNode list:lists){
-            while(list != null){
-                arr.add(list.val);
-                list = list.next;
+        for (ListNode list : lists){
+            if(list != null) {
+              pq.add(list);
             }
         }
         
-        Collections.sort(arr);
-        for(int i : arr){
-            temp.next = new ListNode(i);
+        while(!pq.isEmpty()){
+            temp.next = pq.poll();
             temp = temp.next;
+        
+            if (temp.next != null) pq.add(temp.next);
         }
+        
         return head.next;
     }
 }
