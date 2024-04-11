5d40a0a1d256307381789738aa61144c2adbe1b9
jinhyeok <skusus123@gmail.com>
2024-04-11 13:59:14 +0900

    Time: 1 ms (55.88%), Space: 41.7 MB (53.65%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,16 +1,17 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        int answer = 0;
-        Map<Character, Integer> map = new HashMap<>();
+        int count = 0;
+        Set<Character> set = new HashSet<>();
+        
+        for(char jewel: jewels.toCharArray()){
+            set.add(jewel);
+        }
+        
         for(char stone : stones.toCharArray()){
-            map.put(stone, map.getOrDefault(stone, 0) + 1);
+            if(set.contains(stone))
+               count++;
         }
         
-        for(char jewel : jewels.toCharArray()){
-            if(map.containsKey(jewel))
-                answer += map.get(jewel);
-        }
-        
-        return answer;
+        return count;
     }
 }
