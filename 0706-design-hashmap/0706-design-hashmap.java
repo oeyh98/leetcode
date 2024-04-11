e370ee0c38663757bd5762ddc4aa1d14fd65f26d
jinhyeok <skusus123@gmail.com>
2024-04-11 13:41:51 +0900

    Time: 18 ms (67.63%), Space: 55.1 MB (26.74%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,21 +1,75 @@
 class MyHashMap {
-    int[] hashArr;
+    public class Node {
+        int key, value;
+        Node next;
     
-    public MyHashMap() {
-        hashArr = new int[1000001];
-        Arrays.fill(hashArr, -1);
+        Node(int key, int value){
+            this.key = key;
+            this.value = value;
+        }
     }
     
+    final Node[] nodes = new Node[1000001];
+    
     public void put(int key, int value) {
-        hashArr[key] = value;
+        int index = key % nodes.length;
+        
+        if(nodes[index] == null){
+            nodes[index] = new Node(key, value);
+            return;
+        }
+        
+        Node node = nodes[index];
+        
+        while (node != null){
+            if(node.key == key){
+                node.value = value;
+                return;
+            }
+            if(node.next == null)
+                break;
+            node = node.next;
+        }
+        
+        node.next = new Node(key, value);
     }
     
     public int get(int key) {
-        return hashArr[key];
+        int index = key % nodes.length;
+        Node node = nodes[index];
+        if(node == null)
+            return -1;
+        
+        while(node != null){
+            if(node.key == key)
+                return node.value;
+            node = node.next;
+        }
+        return -1;
     }
     
     public void remove(int key) {
-        hashArr[key] = -1;
+        int index = key % nodes.length;
+        Node node = nodes[index];
+        
+        if(node == null)
+            return;
+        
+        if(node.key == key){
+            if(node.next == null)
+                nodes[index] = null;
+            else
+                nodes[index] = node.next;
+        }
+        Node prev = node;
+        while(node != null){
+            if(node.key == key){
+                prev.next = node.next;
+                return;
+            }
+            prev = node;
+            node = node.next;
+        }
     }
 }
 
